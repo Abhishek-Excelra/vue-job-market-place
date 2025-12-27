9f20bec06c71f4ad247c55a30af19d084a8b032d
Abhishek-Excelra <abhishek.bhavsar@excelra.com>
2025-12-27 12:43:28 +0530

    Add Header component and configure vite dev server polling

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
-<script setup></script>
+<script setup>
+import Header from './components/Layout/Header.vue'
+</script>
 
 <template>
   <h2>Hello Vue.Js</h2>
-  <p>Vue is a progressive framework for building user interfaces.</p>
+  <p>MYYYY Vue is a progressive framework for building user interfaces.</p>
   <p class="heading">Styling with Vue.Js</p>
+
+  <Header />
 </template>
 
 <style scoped>
---
+++ src/components/Layout/Header.vue
@@ -0,0 +1,8 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+<script setup>
+const title = ' My Header'
+</script>
