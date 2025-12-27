83208baeb532bd0a7e42b9189fabb24d6c289ef3
Abhishek-Excelra <abhishek.bhavsar@excelra.com>
2025-12-28 00:58:47 +0530

    Form handling with v-model and submit prevention

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup>
 // import OptionsApp from './components/OptionsApp.vue';
 // import CompositionAppLong from './components/CompositionAppLong.vue';
-import CompositionAppShort from './components/CompositionAppShort.vue';
-
+// import CompositionAppShort from './components/CompositionAppShort.vue';
+import FormComposition from './components/FormComposition.vue';
 </script>
 
 <template>
   <!-- <OptionsApp /> -->
   <!-- <CompositionAppLong /> -->
-  <CompositionAppShort />
+  <!-- <CompositionAppShort /> -->
+   <FormComposition />
 </template>
---
+++ src/components/FormComposition.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue';
+
+const name = ref('Abhishek');
+const email = ref('abhishek@gmail.com');
+
+const submitForm = () => {
+  alert(`${name.value}, ${email.value}`);
+};
+</script>
+
+<template>
+  <form @submit.prevent="submitForm">
+    <label for="name">Name : </label>
+    <input type="text" id="name" v-model="name" />
+    <br />
+    <br />
+    <label for="email">Email : </label>
+    <input type="email" id="email" v-model="email" />
+    <br />
+    <br />
+    <button type="submit">Submit</button>
+  </form>
+</template>
