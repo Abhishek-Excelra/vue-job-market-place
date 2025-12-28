5093c42226626c03217c799e5b087651fb64d995
Abhishek-Excelra <abhishek.bhavsar@excelra.com>
2025-12-29 02:00:43 +0530

    Add back to home button on 404 page with styling

--- src/views/NoteFound.vue
+++ src/views/NoteFound.vue
@@ -4,6 +4,10 @@
   <div class="not-found">
 
     <img src="/not-found.png" alt="404">
+
+    <RouterLink to="/">
+      <button class="not-found__btn"> Back to Home </button>
+    </RouterLink>
   </div>
 </template>
 
@@ -14,5 +18,12 @@
     display: flex;
     justify-content: center;
     align-items: center;
+  }
+  .not-found__btn {
+    background-color: #42b983;
+    color: #fff;
+    padding: 1rem 2rem;
+    border: none;
+    border-radius: 4px;
   }
 </style>
